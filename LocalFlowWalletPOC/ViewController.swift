3bf7d5cdac6653cef15e78f77215b59970110e74
Riccardo Scanavacca <riccardo.scanavacca@icloud.com>
2018-02-10 14:14:44 +0000

    Initial Commit

---
+++ LocalFlowWalletPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalFlowWalletPOC
+//
+//  Created by Riccardo Scanavacca on 10/02/2018.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
